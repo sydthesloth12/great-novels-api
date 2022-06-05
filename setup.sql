a3fb68304250d30c2c54db6d2caca64b61c3aaa8
Sydney <sydneysirois17@gmail.com>
2022-06-05 17:07:36 -0400

    tables added in SQL

--- setup.sql
+++ setup.sql
@@ -15,3 +15,30 @@ CREATE TABLE authors (
 	deletedAt DATETIME,
     primary key(id)
 );
+
+
+CREATE TABLE books (
+	id INT auto_increment,
+    title VARCHAR(225),
+    createdAt DATETIME DEFAULT NOW(),
+	updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+	deletedAt DATETIME,
+	PRIMARY KEY(id),
+    authorId INT, 
+    FOREIGN KEY (authorId) REFERENCES authors(id)
+);
+
+CREATE TABLE genres (
+	id int auto_increment,
+    genreType VARCHAR(225),
+    primary key(id)
+);
+
+CREATE TABLE booksGenres (
+	id int auto_increment,
+    bookId INT,
+    genreId INT, 
+    primary key (id),
+    foreign key (bookId) references books(id),
+    foreign key (genreId) references genres(id)
+ );
