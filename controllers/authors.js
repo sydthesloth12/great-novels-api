37feb02b25c3381ad10f7dbb8ed95ac576b287d5
Sydney <sydneysirois17@gmail.com>
2022-06-11 16:55:33 -0400

    function written server error

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,42 +1,49 @@
 const models = require('../models')
 
-const getAuthors = async (req, res) => {
+const getAuthorBySearchTermWithBooksGenres = async (req, res) => {
   try {
-    const { authors } = req.params
-
-    const author = await models.authors.findAll()
-
-    return author
-      ? res.send(author)
-      : res.sendStatus(404)
-  } catch (error) {
-    return res.sendStatus(500)
-  }
-}
-
-const getAuthorByLastNameFuzzy = async (req, res) => {
-  try {
-    const { lastName } = req.params
-
-    if (!lastName) return res.sendStatus(404)
-
-    const foundAuthor = await models.authors.findOne({
-      attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] },
+    const { searchTerm } = req.params
+    const author = await models.authors.findAll({
       where: {
-        lastName: { [models.Op.like]: `%${lastName}%` }
+        [models.Op.or]: [
+          { nameLast: { [models.Op.like]: `%${searchTerm}%` } },
+          { id: searchTerm },
+        ]
       },
-      include: [{ model: models.books, attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] } }]
+      include: [{
+        model: models.books,
+        include: [{
+          model: models.genres,
+        }]
+      }]
     })
 
-
-
-    if (!foundAuthor) return res.sendStatus(404)
-
-    return res.send(foundAuthor)
+    return res.send(author)
+      ? res.status(200).send(author)
+      : res.sendStatus(404)
   } catch (error) {
     console.log(error)
     return res.sendStatus(500)
   }
 }
 
-module.exports = { getAuthors, getAuthorByLastNameFuzzy }
+const getAuthorById = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    if (!id) return res.sendStatus(404)
+
+    const foundAuthor = await models.authors.findOne({
+      where: { id },
+    })
+
+
+    if (!foundAuthor) return res.sendStatus(404)
+
+    return res.send(foundAuthor)
+  } catch (error) {
+    return res.sendStatus(500)
+  }
+}
+
+module.exports = { getAuthorById, getAuthorBySearchTermWithBooksGenres }
